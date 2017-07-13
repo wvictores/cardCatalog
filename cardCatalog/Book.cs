a080b58d3b2e2bae0f2797917654b308c6663ce0
wvictores <wvictores@gmail.com>
2017-07-13 15:08:25 -0500

    added book

---
+++ cardCatalog/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardCatalog
+{
+    public class Book
+    {
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string title { get; set; }
+    }
+}
