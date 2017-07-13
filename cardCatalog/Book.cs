f72587c9b1a14dd261d9ebd2e9a3719f55d048ed
wvictores <wvictores@gmail.com>
2017-07-13 16:41:32 -0500

    Created user menu

--- cardCatalog/Book.cs
+++ cardCatalog/Book.cs
@@ -8,10 +8,25 @@ namespace cardCatalog
 {
     public class Book
     {
+        /*
+         * Purpose: Simple card catalog system
+         * 
+         * Programmers: Wanda, N.S. Clerman
+         * 
+         * Date: 13-Jul-2017
+         */
+
+        // properties
         public string lastName { get; set; }
         public string firstName { get; set; }
         public string title { get; set; }
         public string longISBN { get; set; }
         public string publishYear { get; set; }
+
+       
+
+        //
+        private string filename;
+
     }
 }
