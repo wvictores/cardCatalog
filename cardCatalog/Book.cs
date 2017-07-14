7fd1596a7b8f7dc1b07d52e2415c96137b272044
Norm Clerman <norm.clerman@gmail.com>
2017-07-14 10:15:33 -0700

    Added Save method. Added code to Book for XML Serialization.

--- cardCatalog/Book.cs
+++ cardCatalog/Book.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace cardCatalog
 {
@@ -11,19 +12,34 @@ namespace cardCatalog
         /*
          * Purpose: Simple card catalog system
          * 
-         * Programmers: Wanda, N.S. Clerman
+         * Programmers: Wanda, N. S. Clerman
          * 
          * Date: 13-Jul-2017
+         * 
+         * Revisions
+         * =========
+         * 1) N. S. Clerman, 14-Jul-2017: Add a constructor (required for XML
+         *    Serialization). Add the XmlAttribute to all the properties (this 
+         *    makes the XML more efficient). using System.Xml.Serialization to
+         *    accomplish this.
          */
 
         // properties
+        [XmlAttribute("fname")]
         public string lastName { get; set; }
+        [XmlAttribute("lname")]
         public string firstName { get; set; }
+        [XmlAttribute("title")]
         public string title { get; set; }
+        [XmlAttribute("isbn")]
         public string longISBN { get; set; }
+        [XmlAttribute("year")]
         public string publishYear { get; set; }
 
-       
+        // constructor
+        // this must be present for XML Serialization
+        // it also must be parameterless
+        public Book() {}
 
         //
         private string filename;
--- cardCatalog/Catalog.cs
+++ cardCatalog/Catalog.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace cardCatalog
 {
@@ -39,5 +41,36 @@ namespace cardCatalog
 
             }
 
-    }
+        }  // Main
+
+        public void Save ()
+        {
+            /*
+             * Purpose: Write the books List as an XML file.
+             * 
+             * Author: N. S. Clerman, 14-Jul-2017
+             * 
+             * Note: This code was taken from the following book:
+             *       "C#7 and .NET Core: Modern Cross-Platform Development"
+             *       Mark J. Price, Second Edition, Packt> Publishing 2017.
+             */ 
+            /* 
+             * 1) establish a filestream on which to write the XML file 
+             *    containing the books list.
+             * 2) create the serializer object and serialize the list to the file.
+             * 3) close the stream to release the file lock.
+            */
+            string xmlFilepath = _filename;
+            FileStream xmlStream = File.Create(xmlFilepath);
+            var xs = new XmlSerializer(typeof(List<Book>));
+            xs.Serialize(xmlStream, books);
+            xmlStream.Dispose();
+
+            // Debugging code. to be commented out as necessary
+            WriteLine($"Written {new FileInfo(xmlFilepath).Length} bytes of "
+                + $"XML to {xmlFilepath}");
+            WriteLine();
+
+            WriteLine(File.ReadAllText(xmlFilepath));
+        }
     }
